c75c7aa41a53b6cbdde4d09256d36dee3e1980c6
Florian Golemo <fgolemo@gmail.com>
2014-09-29 23:30:20 +0100

    adjusted colors

--- js/deedlist.js
+++ js/deedlist.js
@@ -1,11 +1,13 @@
 angular.module('karmaMeter')
     .directive('ngDeedList', function (Data, Timer) {
         var buttonStart = function(id) {
-                $("#deed-"+id+" button").removeClass("button-royal").addClass("button-energized");
+                $("#deed-"+id+".good button").removeClass("button-positive").addClass("button-energized");
+                $("#deed-"+id+".bad button").removeClass("button-assertive").addClass("button-energized");
                 $("#deed-"+id+" button i").removeClass("ion-ios7-clock-outline").addClass("ion-loading-b");
             },
             buttonStop = function(id) {
-                $("#deed-"+id+" button").removeClass("button-energized").addClass("button-royal");
+                $("#deed-"+id+".good button").removeClass("button-energized").addClass("button-positive");
+                $("#deed-"+id+".bad button").removeClass("button-energized").addClass("button-assertive");
                 $("#deed-"+id+" button i").removeClass("ion-loading-b").addClass("ion-ios7-clock-outline");
             };
 
@@ -15,11 +17,13 @@ angular.module('karmaMeter')
             templateUrl: 'templates/dir-deedlist.html',
             link: function(scope, elem, attrs) {
                 scope.data = Data;
-                scope.list = attrs.list;
+
                 if (attrs.list == "good") {
                     scope.list = Data.deeds.good;
+                    scope.good = true;
                 } else {
                     scope.list = Data.deeds.bad;
+                    scope.good = false;
                 }
                 scope.toggleTimer = function(id) {
                     var active = Timer.getActive();
